6295fa1168642d7811292dd5ceca4f9c06346ffb
Arnav <arnavs4709@gmail.com>
2026-07-19 11:32:07 +0000

    comparision operators js pt1

---
+++ 01_basics/comparisions.js
@@ -0,0 +1,20 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2>=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log(2 == "2")
+// console.log(2 > "1")
+// console.log(2 < "1")
+
+// console.log(null == 0)
+// console.log(null > 0)
+// console.log(null >= 0) 
+
+// comparison operators are used to compare two values and return a boolean value (true or false) based on the comparison.
+// comaparison op convert convert null to 0 when using >, <, >=, <= but not when using == or !=
+
+// ===
+console.log(2 === "2")

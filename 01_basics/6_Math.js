1f7b230607fb45c33b8d1c3d796cacbae203f68a
Arnav <arnavs4709@gmail.com>
2026-07-21 14:26:40 +0000

    renamed with serial numbers

---
+++ 01_basics/6_Math.js
@@ -0,0 +1,4 @@
+const score =  400
+// we can explicitily define the type of a variable in js using new Number()
+const balance = new Number(1000)
+console.log(balance.toFixed(2)) // method to convert the number to a string with 2 decimal places
